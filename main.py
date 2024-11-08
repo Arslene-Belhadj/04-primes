e821e9cabb033af33b910bb51403772a7d3c7ea9
Arslene-BELHADJ <arslene.belhadj@edu.esiee.fr>
2024-11-08 14:19:04 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
+"""
+importation pour pouvoir utiliser la fonction sqrt
+"""
 from math import sqrt
 
 #### Fonction secondaire
 
-
 def isprime(p):
-
-    # votre code ici
-
-    pass
+    """
+    Vérifie si un nombre entier p est un nombre premier.
+    """
+    if p == 1:
+        return False
+    for i in range(2, int(sqrt(p)) + 1):
+        if p % i == 0:
+            return False
+    return True
 
 #### Fonction principale
 
 
-def main():
+def main ():
 
     # vos appels à la fonction secondaire ici
+    """
+    Fonction principale qui demande à l'utilisateur d'entrer un nombre 
+    et affiche si ce nombre est premier ou non.
+    """
 
     for n in range(100):
         if isprime(n):
